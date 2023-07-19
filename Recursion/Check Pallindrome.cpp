54e21f6e77e5872e4e2abd8c2e9a2683244862c4
Rishabh Yadav <1998rishabh31@gmail.com>
2023-07-19 22:03:21 +0530

    Update Check Pallindrome.cpp

--- Recursion/Check Pallindrome.cpp
+++ Recursion/Check Pallindrome.cpp
@@ -3,9 +3,13 @@ using namespace std;
 
 bool isPallindrome(int array[], int leftIndex, int rightIndex)
 {
+    // return true from here because if we are reaching this condition, that means until now all the check were true 
+    // else second if the condition would have returned false if they were not
+    
     if(leftIndex > rightIndex)
         return true;
-        
+
+    //if not equal, return false
     if(array[leftIndex] != array[rightIndex])
         return false;
         
