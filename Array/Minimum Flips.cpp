b4e8499434df4b3cec7a4f20cde70425107d8c0b
Rishabh Yadav <1998rishabh31@gmail.com>
2023-08-05 09:57:16 +0530

    Create Minimum Flips.cpp

---
+++ Array/Minimum Flips.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void minimumFlips(int array[], int size)
+{
+    for(int i=1;i<size;i++)
+    {
+        if(array[i] != array[i-1])
+        {
+            if(array[i] != array[0])
+                cout<<"From "<<i<<" to ";
+            else
+                cout<<(i-1)<<"\n";
+        }
+    }
+}
+
+
+
+int main()
+{
+    int array[] = {1,1,0,0,0,1,1,0,0,1};
+    int size = sizeof(array)/sizeof(int);
+    minimumFlips(array,size);
+    return 0;
+}
