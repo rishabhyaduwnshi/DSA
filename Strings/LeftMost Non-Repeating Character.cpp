4c20e701422d598326f29ddd3db5f0210647f344
Rishabh Yadav <1998rishabh31@gmail.com>
2023-08-05 19:55:03 +0530

    Create LeftMost Non-Repeating Character.cpp

---
+++ Strings/LeftMost Non-Repeating Character.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int leftmostNonRepeatingCharacter(string str) 
+{
+    int visited[256];
+    memset(visited,256,false);
+    
+    int result = -1;
+    
+    for(int i=str.length()-1;i>=0;i--)
+        visited[str[i]]++;
+        
+    for(int i=0;i<256;i++)
+    {
+        if(visited[str[i]] == 1)
+            return i;
+    }
+    
+    return result;
+    
+}
+
+
+int main()
+{
+    string str = "abcdab";
+    cout<<leftmostNonRepeatingCharacter(str);
+
+}
