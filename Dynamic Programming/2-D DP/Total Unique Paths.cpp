382f6cbdaef6b8c114d3f4873be9a84cee268f18
Rishabh Yadav <1998rishabh31@gmail.com>
2023-09-03 23:38:04 +0530

    Update Total Unique Paths.cpp

--- Dynamic Programming/2-D DP/Total Unique Paths.cpp
+++ Dynamic Programming/2-D DP/Total Unique Paths.cpp
@@ -1,3 +1,4 @@
+//recursive method
 int paths(int i, int j, vector<vector<int>> &results)
 {
         
@@ -20,4 +21,28 @@ int NumberOfPath(int a, int b)
 {
     vector<vector<int>> results(a+1,vector<int>(b+1,-1));
     return paths(a,b,results);
+}
+
+//tabulation method
+int NumberOfPath(int a, int b)
+{
+    vector<vector<int>> results(a+1,vector<int>(b+1,0));
+        
+    for(int i=1;i<=a;i++)
+    {
+        for(int j=1;j<=b;j++)
+        {
+                
+            if(i == 1 && j == 1)
+                results[i][j] = 1;
+            else
+            {
+                int left = results[i-1][j];
+                int right = results[i][j-1];
+                results[i][j] = left+right;
+            }
+        }
+    }
+    
+    return results[a][b];
 }
