3d27c3e3f6b44a7f1b216bc1c59bd9101aa0eca6
Rishabh Yadav <1998rishabh31@gmail.com>
2023-09-04 23:44:42 +0530

    Update Unique Paths with Blocker.cpp

--- Dynamic Programming/2-D DP/Unique Paths with Blocker.cpp
+++ Dynamic Programming/2-D DP/Unique Paths with Blocker.cpp
@@ -1,3 +1,36 @@
+//bottom-up approach
+
+int uniquePaths(int n, int m, vector<vector<int>>& grid) 
+{
+    vector<vector<int>> results(n + 1, vector<int>(m + 1, 0));
+    for (int i = 1; i <= n; i++) 
+    {
+        for (int j = 1; j <= m; j++) 
+        {
+            if (grid[i - 1][j - 1] == 0) 
+            {
+                results[i][j] = 0;
+            } 
+            else 
+            {
+                if (i == 1 && j == 1) 
+                {
+                    results[i][j] = 1;
+                } 
+                else 
+                {
+                    int left = results[i - 1][j] % 1000000007;
+                    int up = results[i][j - 1] % 1000000007;
+                    results[i][j] = (left + up) % 1000000007;
+                }
+            }
+        }
+    }
+    return results[n][m];
+}
+
+
+//recursive path
 int paths(int n, int m, vector<vector<int>> &grid,  vector<vector<int>> &results)
 {
         
