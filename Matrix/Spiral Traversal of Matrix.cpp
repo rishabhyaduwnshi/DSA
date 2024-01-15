53a109dc9a4e3e0c82d2243aa7e6d9f7fca340eb
Rishabh Yadav <1998rishabh31@gmail.com>
2024-01-15 15:27:08 +0530

    Create Spiral Traversal of Matrix.cpp

---
+++ Matrix/Spiral Traversal of Matrix.cpp
@@ -0,0 +1,36 @@
+vector<int> spirallyTraverse(vector<vector<int> > matrix, int r, int c) 
+    {
+        vector<int> result;
+        int top = 0;
+        int bottom = r-1;
+        
+        int left = 0;
+        int right = c-1;
+        
+        while(top <= bottom && left <= right)
+        {
+            for(int i=left;i<=right;i++)
+                result.push_back(matrix[top][i]);
+            top++;
+        
+            for(int i=top;i<=bottom;i++)
+                result.push_back(matrix[i][right]);
+            right--;
+        
+            if(top <= bottom)
+            {
+                for(int i=right;i>=left;i--)
+                    result.push_back(matrix[bottom][i]);
+                bottom--;
+            }
+        
+            if(left <= right)
+            {
+                for(int i=bottom;i>=top;i--)
+                    result.push_back(matrix[i][left]);
+                left++;
+            }
+        }
+        
+        return result;
+    }
