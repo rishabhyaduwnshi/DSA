35d0421ca36cdabe99623bcd8f5d760de2ac89d8
Rishabh Yadav <1998rishabh31@gmail.com>
2024-01-19 19:26:41 +0530

    Update Overlapping Intervals.cpp

--- Array/Overlapping Intervals.cpp
+++ Array/Overlapping Intervals.cpp
@@ -26,3 +26,24 @@ vector<vector<int>> overlappedInterval(vector<vector<int>>& intervals)
         
         return result;
     }
+
+
+
+vector<vector<int>> overlappedInterval(vector<vector<int>>& intervals) 
+    {
+        sort(intervals.begin(),intervals.end());
+        vector<vector<int>> result;
+        int n = intervals.size();
+        
+        for(int i=0;i<n;i++)
+        {
+            //if the result array is empty or the first element of the last row is greater in the array is greater than the last element
+            // of result, then push
+            if(result.empty() || intervals[i][0] > result.back()[1])
+                result.push_back(intervals[i]);
+            else
+                result.back()[1] = max(result.back()[1],intervals[i][1]);
+        }
+        
+        return result;
+    }
