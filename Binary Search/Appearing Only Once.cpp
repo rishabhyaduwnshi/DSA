e8dd47bfd1e3289700fedd1c36d55e691643720c
Rishabh Yadav <1998rishabh31@gmail.com>
2024-06-10 23:49:37 +0530

    Create Appearing Only Once.cpp

---
+++ Binary Search/Appearing Only Once.cpp
@@ -0,0 +1,28 @@
+int singleNonDuplicate(vector<int>& nums) 
+    {
+        int n = nums.size();
+        if(n == 1)
+            return nums[0];
+        int left = 1;
+        int right = nums.size()-2;
+
+        if(nums[0] != nums[1])
+            return nums[0];
+        
+        if(nums[n-1] != nums[n-2])
+            return nums[n-1];
+
+        while(left <= right)
+        {
+            int mid = (left+right)/2;
+            if(nums[mid-1] != nums[mid] && nums[mid] != nums[mid+1])
+                return nums[mid];
+            else if((mid%2 == 0 && nums[mid+1] == nums[mid]) ||
+                    mid%2 == 1 && nums[mid] == nums[mid-1])
+                left = mid+1;
+            else
+                right = mid-1;
+        }
+
+        return -1;
+    }
