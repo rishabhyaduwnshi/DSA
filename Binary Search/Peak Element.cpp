8e0f92ff440139be14d5a855deac673933b894c3
Rishabh Yadav <1998rishabh31@gmail.com>
2024-06-11 22:38:30 +0530

    Create Peak Element.cpp

---
+++ Binary Search/Peak Element.cpp
@@ -0,0 +1,29 @@
+int findPeakElement(vector<int>& nums) 
+    {
+        int n = nums.size();
+
+        if(n == 1)
+            return 0;
+    
+        if(nums[0] > nums[1])
+            return 0;
+        if(nums[n-1] > nums[n-2])
+            return n-1;
+
+        int left = 1;
+        int right = nums.size()-2;
+
+
+        while(left <= right)
+        {
+            int mid = (left+right)/2;
+            if(nums[mid-1] < nums[mid] && nums[mid] > nums[mid+1])
+                return mid;
+            else if(nums[mid-1] < nums[mid])
+                left = mid+1;
+            else
+                right = mid-1;
+        }
+
+        return -1;
+    }
