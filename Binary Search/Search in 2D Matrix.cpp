ad0bbb9efac4fa9feb641cacc6b213bb33555927
Rishabh Yadav <1998rishabh31@gmail.com>
2024-06-17 16:19:01 +0530

    Update Search in 2D Matrix.cpp

--- Binary Search/Search in 2D Matrix.cpp
+++ Binary Search/Search in 2D Matrix.cpp
@@ -1,34 +1,23 @@
-bool is_present(vector<int> array, int m, int target)
-    {
-        if(array[0] > target || array[m-1] < target)
-            return false;
-
-        int start = 0;
-        int end = m-1;
-
-        while(start <= end)
-        {
-            int mid = (start+end)/2;
-            if(array[mid] == target)
-                return true;
-            else if(array[mid] > target)
-                end = mid-1;
-            else
-                start = mid+1;
-        }
-
-        return false;
-    }
-
-    bool searchMatrix(vector<vector<int>>& matrix, int target) 
+bool searchMatrix(vector<vector<int>>& matrix, int target) 
     {
         int n = matrix.size();
         int m = matrix[0].size();
 
-        for(int i=0;i<n;i++)
+        int start = 0;
+        int end = (n*m)-1;
+
+        while(start <= end)
         {
-            if(is_present(matrix[i],m,target) == true)
+            int mid = (start+end)/2;
+            int i = mid/m;
+            int j = mid%m;
+
+            if(matrix[i][j] == target)
                 return true;
+            else if(matrix[i][j] > target)
+                end = mid-1;
+            else
+                start = mid+1;
         }
 
         return false;
