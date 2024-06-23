3b6c4e6abf1d7cf9a2530f3f7b0909147033cd4e
Rishabh Yadav <1998rishabh31@gmail.com>
2024-06-23 14:29:46 +0530

    Update Clone a List with Random Pointer.cpp

--- Linked List/Clone a List with Random Pointer.cpp
+++ Linked List/Clone a List with Random Pointer.cpp
@@ -20,3 +20,56 @@ Node* copyRandomList(Node* head)
 
         return maps[head];
     }
+
+
+//without using extra space
+Node* restore_origianl_nodes(Node* head)
+    {
+        Node* temp_head = head;
+        Node* result_node = new Node(-1);
+        Node* dummy_node = result_node;
+
+        while(temp_head != nullptr)
+        {
+            dummy_node->next = temp_head->next;
+            temp_head->next = temp_head->next->next;
+            dummy_node = dummy_node->next;
+            temp_head = temp_head->next;
+        }
+        return result_node->next;
+    }
+    void copy_random_ptrs(Node* head)
+    {
+        Node* temp_head = head;
+        while(temp_head != nullptr)
+        {
+            Node* copy_node = temp_head->next;
+            if(temp_head->random != nullptr)
+                copy_node->random = temp_head->random->next;
+            else
+                copy_node->random = nullptr;
+            temp_head = temp_head->next->next;
+        }
+    }
+
+    void insert_nodes(Node* head)
+    {
+        Node* temp_head = head;
+        while(temp_head != nullptr)
+        {
+            Node* new_node = new Node(temp_head->val);
+            new_node->next = temp_head->next;
+            temp_head->next = new_node;
+            temp_head = temp_head->next->next;
+        }
+
+    }
+
+    Node* copyRandomList(Node* head) 
+    {
+        Node* temp_head = head;
+        insert_nodes(temp_head);
+        copy_random_ptrs(temp_head);
+        Node* cloned_head = restore_origianl_nodes(head);
+        return cloned_head;
+    }
